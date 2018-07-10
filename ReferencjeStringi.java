4921e4238831190689e39329e98f8e9a76c875b3
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2018-07-10 09:37:17 +0200

    Create ReferencjeStringi.java

---
+++ ReferencjeStringi.java
@@ -0,0 +1,19 @@
+
+package referencje;
+
+/**
+ *
+ * @author Shabbadoo
+ */
+public class Referencje {
+
+    public static void main(String[] args) {
+
+        String name = new String ("Jakub");
+        String name2 = new String ("Jakub");
+        
+        if(name.equals(name2))
+            System.out.println("Are equal");
+    }
+    
+}
